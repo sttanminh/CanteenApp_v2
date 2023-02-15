fbc1acb3e2574d9346dd677e88f97670910e4b09
Tan Nguyenh <tngu0235@student.monash.edu>
2023-02-15 21:33:32 +1100

    new demo 2

--- src/pages/Canteen/CanteenUpdateSchedule.js
+++ src/pages/Canteen/CanteenUpdateSchedule.js
@@ -1,22 +1,107 @@
 import '../../general.scss';
 import '../../css/CanteenUpdateSchedule.scss';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,useLocation } from 'react-router-dom';
+import db from '../../firebase'
+import { onValue, ref ,set} from "firebase/database";
+import {useState,useEffect,useRef} from 'react'
 
 function CanteenUpdateSchedule() {
 
+
+
+  const accountRef = ref(db, "/account")
+  const [accounts, setAccount] = useState([])
   const navigate = useNavigate();
-  let demeDayList = ["1","2","3","4","5"]
+  const location = useLocation();
+  let [daylist, setDaylist] = useState([])
+  let id
+  
+ 
 
 
-  function listingDay(days){
+
+  function getDays() {
+    console.log(location.state.id)
+    for (let i = 0; i < accounts.length; i++) {
+      if (accounts[i].id == location.state.id){
+        if (accounts[i].days == undefined ){
+          setDaylist([])
+        }
+        else{
+          setDaylist(accounts[i].days)
+        }
+    }
+  }
+}
+
+function updateDays(id,date){
+
+  let day = {
+    date: date,
+    foods: []
+  }
+
+  console.log(accounts)
+  for (let i = 0; i < accounts.length; i++) {
+    if (accounts[i].id === location.state.id){
+      const acc = ref(db, "/account/" + i )
+      if (accounts[i].days == undefined){
+        let temp_days = [day]
+        accounts[i].days = temp_days
+        set(acc,accounts[i])
+
+      }
+      else{
+        accounts[i].days.push(day)
+        set(acc,accounts[i])
+      }
+    } 
+  }
+
+}
+
+  
+  useEffect(() => {
+    if (accounts){
+      setAccount([])
+    }
+    const query = ref(db, "account");
+    return onValue(query, (snapshot) => {
+      const data = snapshot.val();
+      let temp_list = []
+      if (snapshot.exists()) {
+        Object.values(data).map((acc) => {
+          console.log(acc)
+          temp_list.push(acc)
+          setAccount(temp_list);
+        });
+       
+      }
+      temp_list = []
+    });
+    
+    
+    }, []);
+
+
+    useEffect(()=>{
+      getDays()
+    },[accounts])
+
+  
+
+  function listingDay(){
+
     return(
       
-
-      days.map(i=>
+      
+      daylist.map((i, index)=>
             <li>
                 <button onClick={()=>{
-        navigate('/canteenfoodorder')
-      }}> {i}</button>
+                  console.log(location.state.id)
+                  console.log(index)
+                  navigate('/canteenfoodorder', {state:{id: location.state.id, date: i.date, dateIndex: index}})
+      }}> {i.date}</button>
             </li>
         )
 
@@ -26,11 +111,14 @@ function CanteenUpdateSchedule() {
     return (
       
       <div className='canteenUpdateSchedule'>
+        <button id="add" onClick={()=>{
+          updateDays(id, "03/09/2023")
+        }}> + </button>
         <button id="back" onClick={()=>{
-              navigate('/canteendb')
+              navigate('/canteendb', {state:{id: location.state.id}})
             }}> back </button>
            <label>Choose a date</label>
-          {listingDay(demeDayList)}
+          {listingDay()}
       </div>
     );
   }
